6397cd1904b6f27dea388a84356b49506963515e
Ruslan Kuksa <rkuksa@n-ix.com>
2024-06-18 23:37:23 +0300

    Initial Commit

---
+++ FynoTest/FynoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FynoTestApp.swift
+//  FynoTest
+//
+//  Created by Ruslan Kuksa on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FynoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
