e72e311290fc1b716771442e88548767f4a4520a
Ruslan Kuksa <rkuksa@n-ix.com>
2024-07-02 19:07:12 +0300

    Set map offset based on bottom sheet position.

--- FynoTest/Views/ContentView/ContentView.swift
+++ FynoTest/Views/ContentView/ContentView.swift
@@ -11,12 +11,14 @@ import MapKit
 struct ContentView: View {
     
     @State private var model = ContentModel()
+    
     @State private var sheetPresented = true
     @State private var presentationDetent: PresentationDetent = .medium
-    //    @State private var mapPosition: MapCameraPosition = .userLocation(followsHeading: true, fallback: .automatic)
+    @State private var mapPosition: MapCameraPosition = .camera(MapCamera(centerCoordinate: .init(), distance: Double.infinity))
+    @State private var mapOffset: CGFloat = 0
     
     var body: some View {
-        Map {
+        Map(position: $mapPosition) {
             ForEach(model.user.countries) { country in
                 Annotation(country.name, coordinate: country.locationCoordinate) {
                     BubbleAnnotationView(image: country.flag, showBadge: country.visited)
@@ -25,6 +27,8 @@ struct ContentView: View {
             }
         }
         .mapStyle(.imagery(elevation: .realistic))
+        .offset(y: -mapOffset + mapOffset / 2)
+        .contentMargins(.bottom, mapOffset / 2, for: .automatic)
         .task {
             await model.fetchUserData()
         }
@@ -35,21 +39,26 @@ struct ContentView: View {
             }
         }
         .sheet(isPresented: $sheetPresented) {
-            SheetView(model: model)
-                .interactiveDismissDisabled()
-                .presentationDetents([.fraction(0.2), .medium, .fraction(0.7)], selection: $presentationDetent)
-                .presentationBackgroundInteraction(.enabled(upThrough: .medium))
-                .presentationContentInteraction(.resizes)
-                .presentationCornerRadius(25)
-                .presentationDragIndicator(.hidden)
-                .clipShape(
-                    .rect(topLeadingRadius: 16, topTrailingRadius: 16)
-                )
-                .onChange(of: presentationDetent) {
-                    if presentationDetent == .fraction(0.2) {
-                        presentationDetent = .medium
+            GeometryReader { proxy in
+                SheetView(model: model)
+                    .interactiveDismissDisabled()
+                    .presentationDetents([.fraction(0.2), .medium, .fraction(0.7)], selection: $presentationDetent)
+                    .presentationBackgroundInteraction(.enabled(upThrough: .medium))
+                    .presentationBackgroundInteraction(.enabled(upThrough: .fraction(0.7)))
+                    .presentationContentInteraction(.resizes)
+                    .presentationCornerRadius(25)
+                    .presentationDragIndicator(.hidden)
+                    .onChange(of: presentationDetent) {
+                        if presentationDetent == .fraction(0.2) {
+                            presentationDetent = .medium
+                        }
                     }
-                }
+                    .onChange(of: proxy.size) {
+                        withAnimation {
+                            mapOffset = proxy.size.height
+                        }
+                    }
+            }
         }
     }
 }
